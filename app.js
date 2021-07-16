58c233d42e025d5f2831385cc481c127e9962c5b
Shane <S.frenchko@gmail.com>
2021-07-16 12:27:19 -0400

    Added first.txt

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const http = require('http');
+const {
+    readFile
+} = require('fs');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Welcome to our home page');
-    }
-    if (req.url === '/about') {
-        res.end('Here is our short story');
-    }
-    res.end('<h1>Oops!</h1><p>We can\'t seem to find the page you are looking for</p><a href="about">back home</a>')
-})
-
-server.listen(3000);
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        
+        readFile('./first.txt', 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            };
+        });
+    })
+}
+getText('./first.txt').then((result)=> {console.log(result)}).catch((err) => {console.log(err)})
